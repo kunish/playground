659910e3fe518869c067c2167c82be181e35eb7f
shikun <kunish.butt@gmail.com>
2021-08-14 17:56:40 +0800

    fix: typo in vapp.tf

--- vapp.tf
+++ vapp.tf
@@ -3,7 +3,7 @@ resource "vsphere_vapp_container" "cluster" {
   parent_resource_pool_id = data.vsphere_resource_pool.pool.id
 }
 
-resource "vsphere_vapp_entity" "entitiess_cluster" {
+resource "vsphere_vapp_entity" "entities_cluster" {
   for_each     = vsphere_virtual_machine.vms_cluster
   container_id = vsphere_vapp_container.cluster.id
   target_id    = each.value.moid
