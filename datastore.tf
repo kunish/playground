aa5898fed40f89ee9c0862638b6df17c53dea5e9
shikun <kunish.butt@gmail.com>
2021-08-24 04:32:44 +0800

    feat: use depends on to get data resource

--- datastore.tf
+++ datastore.tf
@@ -2,4 +2,8 @@ data "vsphere_datastore" "datastore" {
   name = "esxi"
 
   datacenter_id = vsphere_datacenter.datacenter.moid
+
+  depends_on = [
+    vsphere_host.emc
+  ]
 }
--- resource_pool.tf
+++ resource_pool.tf
@@ -2,4 +2,8 @@ data "vsphere_resource_pool" "emc_pool" {
   name = "esxi.kuin.sh/Resources"
 
   datacenter_id = vsphere_datacenter.datacenter.moid
+
+  depends_on = [
+    vsphere_host.emc
+  ]
 }
