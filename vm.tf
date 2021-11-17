a14dec658abf1705aa73bc288d2f0a8b4b4dce99
kunish <kunish.butt@gmail.com>
2021-11-17 22:13:04 +0800

    feat: add staging cluster virtual machines

--- vm.tf
+++ vm.tf
@@ -99,3 +99,37 @@ module "vm_cluster_worker" {
 
   vms = ["cluster-worker-1", "cluster-worker-2", "cluster-worker-3", "cluster-worker-4", "cluster-worker-5"]
 }
+
+module "vm_cluster_controlplane_staging" {
+  source = "./modules/vm-ova-ubuntu"
+
+  folder           = "staging"
+  resource_pool_id = data.vsphere_resource_pool.emc_pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  template_uuid    = vsphere_content_library_item.ubuntu.id
+  guest_id         = "ubuntu64Guest"
+  num_cpus         = 4
+  memory           = 4096
+  network_id       = vsphere_distributed_port_group.dpg.id
+  disk_size        = 64
+  vapp_seedfrom    = "http://pi1.kuin.sh/homelab/"
+
+  vms = ["cluster-controlplane-staging"]
+}
+
+module "vm_cluster_worker_staging" {
+  source = "./modules/vm-ova-ubuntu"
+
+  folder           = "staging"
+  resource_pool_id = data.vsphere_resource_pool.emc_pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  template_uuid    = vsphere_content_library_item.ubuntu.id
+  guest_id         = "ubuntu64Guest"
+  num_cpus         = 8
+  memory           = 8192
+  network_id       = vsphere_distributed_port_group.dpg.id
+  disk_size        = 128
+  vapp_seedfrom    = "http://pi1.kuin.sh/homelab/"
+
+  vms = ["cluster-worker-1-staging", "cluster-worker-2-staging", "cluster-worker-3-staging"]
+}
