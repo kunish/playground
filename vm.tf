f1f0a86bc872fb06c1c41342497c001199e474ec
kunish <kunish.butt@gmail.com>
2021-11-20 19:29:29 +0800

    feat: drop workstation

--- vm.tf
+++ vm.tf
@@ -32,23 +32,6 @@ module "vm_mini" {
   vms = ["speedtest"]
 }
 
-module "vm_large" {
-  source = "./modules/vm-ova-ubuntu"
-
-  folder           = "production"
-  resource_pool_id = data.vsphere_resource_pool.emc_pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  template_uuid    = vsphere_content_library_item.ubuntu.id
-  guest_id         = "ubuntu64Guest"
-  num_cpus         = 16
-  memory           = 16384
-  network_id       = vsphere_distributed_port_group.dpg.id
-  disk_size        = 128
-  vapp_seedfrom    = "http://pi1.kuin.sh/homelab/"
-
-  vms = ["workstation"]
-}
-
 module "vm_cluster_proxy" {
   source = "./modules/vm-ova-ubuntu"
 
